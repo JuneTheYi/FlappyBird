83148850872b86a0d66d0414f5d0230e53e8b039
JuneTheYi <JuneTheYi@gmail.com>
2021-11-07 22:38:24 -0500

    loaded images and processing.core jar file

---
+++ src/FlappyBird.java
@@ -0,0 +1,8 @@
+import processing.core.PApplet; // downloaded processing from processing.org
+import processing.core.PImage;
+// imported this through File, Project Structure, Libraries, added core.jar from processing library
+
+public class FlappyBird extends PApplet{
+    PImage backpic, birdpic, wallpic, welcomescreen;
+    backpic =loadImage("https://raw.githubusercontent.com/junetheyi/flappybird/")
+}
